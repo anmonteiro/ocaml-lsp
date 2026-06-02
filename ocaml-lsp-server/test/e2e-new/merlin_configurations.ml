3d28f7e534ae715182cd36db5ea917069e2709fb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-06-01 20:24:24 -0700

    test(single-context): keep Merlin selection per document
    Signed-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>

--- ocaml-lsp-server/test/e2e-new/merlin_configurations.ml
+++ ocaml-lsp-server/test/e2e-new/merlin_configurations.ml
@@ -6,10 +6,13 @@ module Merlin_call = Ocaml_lsp_server.Custom_request.Merlin_call_compatible
 
 let uri = DocumentUri.of_path "test.ml"
 let text_document = TextDocumentIdentifier.create ~uri
+let other_uri = DocumentUri.of_path "other.ml"
+let other_text_document = TextDocumentIdentifier.create ~uri:other_uri
 
 let fake_ocaml_merlin_exe () =
   let cwd = Sys.getcwd () in
   [ "fake_ocaml_merlin/fake_ocaml_merlin.exe"
+  ; "../../../_build/default/ocaml-lsp-server/test/e2e-new/fake_ocaml_merlin/fake_ocaml_merlin.exe"
   ; "_build/default/ocaml-lsp-server/test/e2e-new/fake_ocaml_merlin/fake_ocaml_merlin.exe"
   ; "ocaml-lsp/ocaml-lsp-server/test/e2e-new/fake_ocaml_merlin/fake_ocaml_merlin.exe"
   ; "_build/default/ocaml-lsp/ocaml-lsp-server/test/e2e-new/fake_ocaml_merlin/fake_ocaml_merlin.exe"
@@ -47,7 +50,7 @@ let print_response_or_error request =
       Exn_with_backtrace.to_dyn error |> Dyn.to_string |> print_endline)
 ;;
 
-let merlin_configurations client =
+let merlin_configurations ?(text_document = text_document) client =
   let params =
     Configurations.Request_params.create ~text_document
     |> Configurations.Request_params.yojson_of_t
@@ -55,7 +58,7 @@ let merlin_configurations client =
   request client ~meth:Configurations.meth ~params
 ;;
 
-let select_merlin_configuration client id =
+let select_merlin_configuration ?(text_document = text_document) client id =
   let params =
     Select.Request_params.create ~text_document ~id
     |> Select.Request_params.yojson_of_t
@@ -63,7 +66,7 @@ let select_merlin_configuration client id =
   request client ~meth:Select.meth ~params
 ;;
 
-let merlin_errors client =
+let merlin_errors ?(text_document = text_document) client =
   let params =
     Merlin_call.Request_params.create
       ~text_document
@@ -142,6 +145,34 @@ let%expect_test "new protocol: selected configuration affects existing requests"
       "result": "{\"class\":\"return\",\"value\":[{\"start\":{\"line\":1,\"col\":9},\"end\":{\"line\":1,\"col\":39},\"type\":\"warning\",\"sub\":[],\"valid\":true,\"message\":\"Warning 8: this pattern-matching is not exhaustive.\\n  Here is an example of a case that is not matched: Some _\"}]}"
     }
     { "resultAsSexp": false, "result": "{\"class\":\"return\",\"value\":[]}" }
+    |}]
+;;
+
+let%expect_test "new protocol: selection is per document" =
+  let request client =
+    let open Fiber.O in
+    let textDocument =
+      TextDocumentItem.create ~uri:other_uri ~languageId:"ocaml" ~version:0 ~text:source
+    in
+    let* () =
+      Client.notification
+        client
+        (TextDocumentDidOpen (DidOpenTextDocumentParams.create ~textDocument))
+    in
+    let* (_ : Yojson.Safe.t) = select_merlin_configuration client "melange" in
+    let* response = merlin_errors client in
+    Test.print_result response;
+    let+ response = merlin_errors ~text_document:other_text_document client in
+    Test.print_result response
+  in
+  Helpers.test ~extra_env:(extra_env "new") source request;
+  [%expect
+    {|
+    { "resultAsSexp": false, "result": "{\"class\":\"return\",\"value\":[]}" }
+    {
+      "resultAsSexp": false,
+      "result": "{\"class\":\"return\",\"value\":[{\"start\":{\"line\":1,\"col\":9},\"end\":{\"line\":1,\"col\":39},\"type\":\"warning\",\"sub\":[],\"valid\":true,\"message\":\"Warning 8: this pattern-matching is not exhaustive.\\n  Here is an example of a case that is not matched: Some _\"}]}"
+    }
     |}]
 ;;
 
